c7db52bfeae88374df07a6a98e990a1c475f83cc
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-16 16:11:58 -0500

    create small card web component

---
+++ src/components/small-card/smallCard.js
@@ -0,0 +1,33 @@
+class smallCard extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+
+        var socialIcon = this.getAttribute('icon');
+        var textActivity = this.getAttribute('text-activity');
+        var number = this.getAttribute('number');
+        var iconUpDown = this.getAttribute('icon-up-down');
+        var numberUpDown = this.getAttribute('number-up-down');
+        var colorUpDown;
+
+        if (iconUpDown == "down") {
+            colorUpDown = "red" 
+        }
+
+        this.innerHTML = `
+            <div class="small-card">
+                <span class="text-activity">${textActivity}</span>
+                <img src="../../src/images/icon-${socialIcon}.svg" alt="icono ${socialIcon}" >
+                <div class="number">${number}</div>
+                <div class="date ${colorUpDown}">
+                    <img src="../../src/images/icon-${iconUpDown}.svg" alt="icono ${iconUpDown}" >
+                    <span>${numberUpDown}%</span>
+                </div>
+            </div>
+        `;   
+    }
+}
+
+customElements.define('small-card', smallCard);
