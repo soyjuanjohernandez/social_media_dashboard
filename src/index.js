ecd80447d83453f2fc4fe61d015fe856b30bfffe
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-16 23:13:28 -0500

    add attribution

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ var checkbox = document.querySelector('.input-switch');
 
 checkbox.addEventListener('change', function() {
     if(this.checked) {
-        // alert("hola");
-        // trans()
         document.documentElement.setAttribute('data-theme', 'dark')
     } else {
-        // trans()
-        // alert("hola2");
         document.documentElement.setAttribute('data-theme', 'light')
     }
 })
 
-// let trans = () => {
-//     document.documentElement.classList.add('transition');
-//     window.setTimeout(() => {
-//         document.documentElement.classList.remove('transition');
-//     }, 1000)
-// }
